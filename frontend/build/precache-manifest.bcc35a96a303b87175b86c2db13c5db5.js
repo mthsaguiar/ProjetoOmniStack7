e196daa52f827fae18b72af7d8ca051c1ce92f6a
Matheus Aguiar <mths.aguiarr@gmail.com>
2019-09-27 20:15:47 -0300

    Just updating git repo

---
+++ frontend/build/precache-manifest.bcc35a96a303b87175b86c2db13c5db5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d07b1e13f54510c5908588096bb14e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b734aecdb223d00573db",
+    "url": "/static/css/main.bd999c60.chunk.css"
+  },
+  {
+    "revision": "50ced58231e5545c4c3d",
+    "url": "/static/js/2.57897ca4.chunk.js"
+  },
+  {
+    "revision": "b734aecdb223d00573db",
+    "url": "/static/js/main.e6043668.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "83fd0bcc6cdc21e671ae5edf856ba3ef",
+    "url": "/static/media/logo.83fd0bcc.svg"
+  }
+]);
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+import { Link } from 'react-router-dom';
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+        <div className="header-content">
+            <Link to="/">
+                <img src={logo} alt="RocketInsta"/>
+            </Link>
+            <Link to="/new">
+                <img src={camera} alt="EnviarPublic"/>
+            </Link>
+
+        </div>
+    </header>
+    
+  );
+}
